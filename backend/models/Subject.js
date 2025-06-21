7e9589562ba803d0ba2de345ecdd741269b0abbe
KUSHAN A WICKRAMASINGHE <tech88125@gmail.com>
2025-06-21 18:20:07 +0530

    Update Subject.js

--- backend/models/Subject.js
+++ backend/models/Subject.js
@@ -1,1 +1,35 @@
+const db = require('../config/db');
 
+const Subject = {
+  getAll: (callback) => {
+    db.query('SELECT * FROM subjects ORDER BY name ASC', callback);
+  },
+
+  getById: (id, callback) => {
+    db.query('SELECT * FROM subjects WHERE id = ?', [id], callback);
+  },
+
+  create: (data, callback) => {
+    const { name, grade } = data;
+    db.query(
+      'INSERT INTO subjects (name, grade) VALUES (?, ?)',
+      [name, grade],
+      callback
+    );
+  },
+
+  update: (id, data, callback) => {
+    const { name, grade } = data;
+    db.query(
+      'UPDATE subjects SET name = ?, grade = ? WHERE id = ?',
+      [name, grade, id],
+      callback
+    );
+  },
+
+  delete: (id, callback) => {
+    db.query('DELETE FROM subjects WHERE id = ?', [id], callback);
+  },
+};
+
+module.exports = Subject;
