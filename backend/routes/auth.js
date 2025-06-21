aad63df5a5d58d2658c0941632c1d74d6c94c6d0
KUSHAN A WICKRAMASINGHE <tech88125@gmail.com>
2025-06-21 18:13:36 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
 
+// @route   POST /api/auth/register
+// @desc    Register user (Admin / Teacher / Student)
+router.post('/register', authController.register);
+
+// @route   POST /api/auth/login
+// @desc    Login user (Admin / Teacher / Student)
+router.post('/login', authController.login);
+
+module.exports = router;
