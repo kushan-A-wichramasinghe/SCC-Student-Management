e4120383de653e71033bd0f5a8413d54ff38d533
pakaya0704 <tech88125@gmail.com>
2025-06-21 17:09:55 +0530

    Update Student.js

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -1,1 +1,31 @@
+const db = require('../config/db');
+
+const Student = {
+  getAll: (callback) => {
+    const sql = 'SELECT * FROM students';
+    db.query(sql, callback);
+  },
+
+  getById: (id, callback) => {
+    const sql = 'SELECT * FROM students WHERE id = ?';
+    db.query(sql, [id], callback);
+  },
+
+  create: (data, callback) => {
+    const sql = 'INSERT INTO students (name, email, class, section) VALUES (?, ?, ?, ?)';
+    db.query(sql, [data.name, data.email, data.class, data.section], callback);
+  },
+
+  update: (id, data, callback) => {
+    const sql = 'UPDATE students SET name = ?, email = ?, class = ?, section = ? WHERE id = ?';
+    db.query(sql, [data.name, data.email, data.class, data.section, id], callback);
+  },
+
+  delete: (id, callback) => {
+    const sql = 'DELETE FROM students WHERE id = ?';
+    db.query(sql, [id], callback);
+  }
+};
+
+module.exports = Student;
 
