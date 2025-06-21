27f32ea707f71bd7af746120b08d5b4862cbaa35
pakaya0704 <tech88125@gmail.com>
2025-06-21 16:48:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const db = require('./config/db');
 
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/students', require('./routes/students'));
+app.use('/api/teachers', require('./routes/teachers'));
+app.use('/api/comments', require('./routes/comments'));
+app.use('/api/subjects', require('./routes/subjects'));
+
+// Test Route
+app.get('/', (req, res) => {
+  res.send('SCC Student Management API is running...');
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
