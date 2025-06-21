2594a1129048eefddbe3c36b62d26e2730fe85cd
pakaya0704 <tech88125@gmail.com>
2025-06-21 17:27:54 +0530

    Update teacherController.js

--- backend/controllers/teacherController.js
+++ backend/controllers/teacherController.js
@@ -1,1 +1,19 @@
+const Teacher = require('../models/Teacher');
 
+exports.getAllTeachers = (req, res) => {
+  Teacher.getAll((err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(results);
+  });
+};
+
+exports.getTeacherById = (req, res) => {
+  Teacher.getById(req.params.id, (err, result) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(result[0]);
+  });
+};
+
+exports.createTeacher = (req, res) => {
+  Teacher.create(req.body, (err, result) => {
+    if (err) return res.status(500).json({ error: err
