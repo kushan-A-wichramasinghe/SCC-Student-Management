a971a37cd7a6d2b31aefaae641f19f9a814ce138
KUSHAN A WICKRAMASINGHE <tech88125@gmail.com>
2025-06-21 18:10:24 +0530

    Update Admin.js

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -1,1 +1,18 @@
+const db = require('../config/db');
 
+const Admin = {
+  getByEmail: (email, callback) => {
+    db.query('SELECT * FROM admins WHERE email = ?', [email], callback);
+  },
+
+  create: (data, callback) => {
+    const { email, password } = data;
+    db.query(
+      'INSERT INTO admins (email, password) VALUES (?, ?)',
+      [email, password],
+      callback
+    );
+  },
+};
+
+module.exports = Admin;
