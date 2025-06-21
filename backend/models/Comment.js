f23f930acbcd02fa011a4f13675314b4c1a4c3bd
KUSHAN A WICKRAMASINGHE <tech88125@gmail.com>
2025-06-21 18:15:39 +0530

    Update Comment.js

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,1 +1,22 @@
+const db = require('../config/db');
 
+const Comment = {
+  getAll: (callback) => {
+    db.query('SELECT * FROM comments ORDER BY created_at DESC', callback);
+  },
+
+  create: (data, callback) => {
+    const { student_id, message } = data;
+    db.query(
+      'INSERT INTO comments (student_id, message) VALUES (?, ?)',
+      [student_id, message],
+      callback
+    );
+  },
+
+  delete: (id, callback) => {
+    db.query('DELETE FROM comments WHERE id = ?', [id], callback);
+  },
+};
+
+module.exports = Comment;
