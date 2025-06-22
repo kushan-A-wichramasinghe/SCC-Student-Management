b8b97eda2aeaea3b207f42cf30b40b3bf76ef549
KUSHAN A WICKRAMASINGHE <tech88125@gmail.com>
2025-06-22 07:24:50 +0530

    Update dashboard.js

--- frontend/assets/js/dashboard.js
+++ frontend/assets/js/dashboard.js
@@ -1,1 +1,10 @@
-
+fetch('/api/dashboard')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById('total-students').innerText = data.total_students;
+    document.getElementById('online-students').innerText = data.online_students;
+    document.getElementById('total-teachers').innerText = data.total_teachers;
+  })
+  .catch(err => {
+    console.error("Dashboard fetch error:", err);
+  });
